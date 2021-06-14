2c84cf4f4bf4af0814b8122278e0f1290e22fd99
Arijan Babickovic <arijanfs@gmail.com>
2021-06-14 19:51:00 +0100

    disabled request forgery protection

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -4,7 +4,7 @@ class ChatroomController < ApplicationController
     def index
         @message = Message.new
         @messages = Message.custom_display # instead of Message.all, scope defined in the Message model
-        @online_users = User.where("last_seen_at > ?", 1.minutes.ago)
+        @online_users = User.where("last_seen_at > ?", 3.minutes.ago)
     end
     
 end
